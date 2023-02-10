46cc0c8ee6af7e169689df169beade1bd61d89a1
Saif-Dawood <saifdawoodcs@gmail.com>
2023-02-10 06:18:12 +0200

    edit database file and contents

--- database/crtables.php
+++ database/crtables.php
@@ -1,32 +1,30 @@
 <?php
+require "crtdb.php";
 
-    require "createdb.php";
+// Connecting to the database
+try {
+    $conn = new mysqli($sname, $uname, $pass, $db);
+} catch (Exception $e) {
+    die("Connection to created database failed: " . $e->getMessage());
+}
 
+// Query to create the table
+$sql = "CREATE TABLE IF NOT EXISTS items (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        sku VARCHAR(12) NOT NULL,
+        name VARCHAR(30) NOT NULL,
+        price REAL(6,2) UNSIGNED NOT NULL ,
+        type VARCHAR(4) NOT NULL,
+        size INT(6) UNSIGNED,
+        weight REAL(3,2) UNSIGNED,
+        dimensions VARCHAR(14))";
 
-    // Connecting to the database
-    try {
-        $conn = new mysqli($sname, $uname, $pass, $db);
-    } catch (Exception $e) {
-        die("Connection to created database failed: " . $e->getMessage());
-    }
+// Creating the table if it doesn't actually exist
+try {
+    $conn->query($sql);
+} catch (Exception $e) {
+    die("Table creatin failed: " . $e->getMessage());
+}
 
-    // Query to create the table
-    $sql = "CREATE TABLE IF NOT EXISTS items (
-            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            sku VARCHAR(12) NOT NULL,
-            name VARCHAR(30) NOT NULL,
-            price REAL(6,2) UNSIGNED NOT NULL ,
-            type VARCHAR(4) NOT NULL,
-            size INT(6) UNSIGNED,
-            weight REAL(3,2) UNSIGNED,
-            dimensions VARCHAR(14))";
-
-    // Creating the table if it doesn't actually exist
-    try {
-        $conn->query($sql);
-    } catch (Exception $e) {
-        die("Table creatin failed: " . $e->getMessage());
-    }
-
-    // saving the process
-    $conn->close();
+// saving the process
+$conn->close();
---
+++ database/crtdb.php
@@ -0,0 +1,26 @@
+<?php
+
+$sname = "localhost";
+$uname = "saif";
+$pass = "qwer";
+$db = "scandi";
+
+// Connecting without a database
+try {
+    $conn = new mysqli($sname, $uname, $pass);
+} catch (Exception $e) {
+    die("Connection failed " . $e->getMessage());
+}
+
+// Query to create the database
+$sql = "CREATE DATABASE IF NOT EXISTS scandi";    
+
+// Creating a new database if it doesn't exist already
+try {
+    $conn->query($sql);
+} catch (Exception $e) {
+    die("Error while creating database: " . $e->getMessage());
+}
+
+// saving the process
+$conn->close();
