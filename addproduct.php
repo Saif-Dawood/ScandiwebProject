d2e30cdedab17207781c2fa50c4ae0dfb5141156
Saif-Dawood <saifdawoodcs@gmail.com>
2023-02-15 15:06:17 +0200

    Refuse negative floating inputs

--- addproduct.php
+++ addproduct.php
@@ -15,10 +15,10 @@ use Vendor\Book;
 use Vendor\Furn;
 
 $skuErr = $nameErr = $priceErr = $typeErr = $sizeErr =
-$weightErr = $heightErr = $widthErr = $lengthErr = $diffErr = "";
+    $weightErr = $heightErr = $widthErr = $lengthErr = $diffErr = "";
 
 $sku = $name = $price = $type = $size = $Attr =
-$weight = $height = $width = $length = $diff = "";
+    $weight = $height = $width = $length = $diff = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // To check if there is an error
@@ -42,7 +42,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $oof = 0;
     } else {
         $price = test_input($_POST['price']);
-    } if (empty($_POST['type'])) {
+    }
+    if (empty($_POST['type'])) {
         $typeErr = "Choose a type";
         $oof = 0;
     } else {
@@ -58,8 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (empty($_POST['weight'])) {
                 $weightErr = "Weight is required";
                 $oof = 0;
-            }
-            else {
+            } else {
                 $weight = test_input($_POST['weight']);
             }
         } else if (array_key_exists('height', $_POST)) {
@@ -108,37 +108,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nameErr = "Only letters and whitespaces are allowed";
         $oof = 0;
     }
-    if (!filter_var($price, FILTER_VALIDATE_FLOAT) && !empty($price)) {
-        $priceErr = "Only decimal numbers are allowed";
+    if ((!filter_var($price, FILTER_VALIDATE_FLOAT) || $price <= 0)
+        && !empty($price)
+    ) {
+        $priceErr = "Only positive decimal numbers are allowed";
         $oof = 0;
     }
     if (array_key_exists('size', $_POST)) {
         // $size = test_input($_POST['size']);
-        if (!filter_var($size, FILTER_VALIDATE_FLOAT) && !empty($size)) {
-            $sizeErr = "Only decimal numbers are allowed";
+        if ((!filter_var($size, FILTER_VALIDATE_FLOAT) || $size <= 0)
+            && !empty($size)
+        ) {
+            $sizeErr = "Only positive decimal numbers are allowed";
             $oof = 0;
         } else {
             $diff = $size;
         }
     } else if (array_key_exists('weight', $_POST)) {
         // $weight = test_input($_POST['weight']);
-        if (!filter_var($weight, FILTER_VALIDATE_FLOAT) && !empty($weight)) {
-            $weightErr = "Only decimal numbers are allowed";
+        if ((!filter_var($weight, FILTER_VALIDATE_FLOAT) || $weight <= 0)
+            && !empty($weight)
+        ) {
+            $weightErr = "Only positive decimal numbers are allowed";
             $oof = 0;
         } else {
             $diff = $weight;
         }
     } else if (array_key_exists('height', $_POST)) {
         // echo "oof";
-        if (!filter_var($height, FILTER_VALIDATE_FLOAT) && !empty($height)) {
+        if ((!filter_var($height, FILTER_VALIDATE_FLOAT) || $height <= 0)
+            && !empty($height)
+        ) {
             // echo "oof";
-            $heightErr = "Only decimal numbers are allowed";
+            $heightErr = "Only positive decimal numbers are allowed";
             $oof = 0;
-        } else if (!filter_var($width, FILTER_VALIDATE_FLOAT) && !empty($width)) {
-            $widthErr = "Only decimal numbers are allowed";
+        } else if ((!filter_var($width, FILTER_VALIDATE_FLOAT)
+            || $width <= 0) && !empty($width)) {
+            $widthErr = "Only positive decimal numbers are allowed";
             $oof = 0;
-        } else if (!filter_var($length, FILTER_VALIDATE_FLOAT) && !empty($length)) {
-            $lengthErr = "Only decimal numbers are allowed";
+        } else if ((!filter_var($length, FILTER_VALIDATE_FLOAT)
+            || $length <= 0) && !empty($length)) {
+            $lengthErr = "Only positive decimal numbers are allowed";
             $oof = 0;
         } else {
             $diff = $height . "x" . $width . "x" . $length;
@@ -173,7 +183,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $arr['widthErr'] = $widthErr;
         $arr['length'] = $length;
         $arr['lengthErr'] = $lengthErr;
-        
+
         $Attr = $ttype::printHtml($arr);
     }
 }
@@ -208,48 +218,48 @@ function test_input($data)
     <form action="addproduct.php" method="post" id="product_form">
 
         <!-- <div class="main"> -->
-            <span class="text-danger">* Required fields</span>
-            <div class="attrib">
-                <label for="sku">SKU: </label>
-                <input type="text" name="sku" id="sku" value="<?=$sku?>">
-                <span for="sku" class="text-danger">
-                    * <?= $skuErr ?>
-                </span>
-            </div>
+        <span class="text-danger">* Required fields</span>
+        <div class="attrib">
+            <label for="sku">SKU: </label>
+            <input type="text" name="sku" id="sku" value="<?= $sku ?>">
+            <span for="sku" class="text-danger">
+                * <?= $skuErr ?>
+            </span>
+        </div>
 
-            <div class="attrib">
-                <label for="name">Name: </label>
-                <input type="text" name="name" id="name" value="<?=$name?>">
-                <span for="name" class="text-danger">
-                    * <?= $nameErr ?>
-                </span>
-            </div>
+        <div class="attrib">
+            <label for="name">Name: </label>
+            <input type="text" name="name" id="name" value="<?= $name ?>">
+            <span for="name" class="text-danger">
+                * <?= $nameErr ?>
+            </span>
+        </div>
 
-            <div class="attrib">
-                <label for="price">Price ($): </label>
-                <input type="text" name="price" id="price" value="<?=$price?>">
-                <span for="price" class="text-danger">
-                    * <?= $priceErr ?>
-                </span>
-            </div>
+        <div class="attrib">
+            <label for="price">Price ($): </label>
+            <input type="text" name="price" id="price" value="<?= $price ?>">
+            <span for="price" class="text-danger">
+                * <?= $priceErr ?>
+            </span>
+        </div>
 
-            <div class="attrib">
-                <label for="type">Type Switcher: </label>
-                <select name="type" id="productType" onchange="displayAttr(this.value)">
-                    <option value="" disabled <?php if ($type == "")echo "selected";?>>
-                        Type Switcher
-                    </option>
-                    <option value="DVD" <?php if ($type == "DVD")echo "selected";?>>DVD-disc</option>
-                    <option value="Book" <?php if ($type == "Book")echo "selected";?>>Book</option>
-                    <option value="Furn" <?php if ($type == "Furn")echo "selected";?>>Furniture</option>
-                </select>
-                <span for="type" class="text-danger">
-                    * <?= $typeErr ?>
-                </span>
-            </div>
-            <div id="Attr">
-                <?= $Attr?>
-            </div>
+        <div class="attrib">
+            <label for="type">Type Switcher: </label>
+            <select name="type" id="productType" onchange="displayAttr(this.value)">
+                <option value="" disabled <?php if ($type == "") echo "selected"; ?>>
+                    Type Switcher
+                </option>
+                <option value="DVD" <?php if ($type == "DVD") echo "selected"; ?>>DVD-disc</option>
+                <option value="Book" <?php if ($type == "Book") echo "selected"; ?>>Book</option>
+                <option value="Furn" <?php if ($type == "Furn") echo "selected"; ?>>Furniture</option>
+            </select>
+            <span for="type" class="text-danger">
+                * <?= $typeErr ?>
+            </span>
+        </div>
+        <div id="Attr">
+            <?= $Attr ?>
+        </div>
 
     </form>
     <footer>Scandiweb Test Assignment</footer>
