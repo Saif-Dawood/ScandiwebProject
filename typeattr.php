0b7e70da3b07dcb5f04386ce115c313436bde229
Saif-Dawood <saifdawoodcs@gmail.com>
2023-10-14 01:46:37 +0300

    [UPDT] Stop rendering HTML by class functions: printHtml() that was used for rendering the extra fields is replaced

--- typeattr.php
+++ typeattr.php
@@ -16,7 +16,26 @@ $type = $_REQUEST["t"];
 $type = "Vendor\\" . $type;
 
 if (class_exists($type))
-    echo $type::printHtml();
+{
+    $obj = new $type("", "", "", array());
+    $html = "";
+    foreach ($obj->print_add as $field)
+    {
+        $html .= <<<HTML
+            <div class="attrib">
+                <label for="{$field['lower']}">{$field['title']}{$field['mu']}: </label>
+                <input type="text" name="{$field['lower']}" id="{$field['lower']}" value="">
+                <span for="{$field['lower']}" class="text-danger">
+                    *
+                </span>
+            </div>
+        HTML;
+    }
+    $html .= <<<HTML
+        <p style="font-weight:bold;">{$obj->print_description}</p>
+    HTML;
+    echo $html;
+}
 else
     echo <<<HTML
     <p style="font-weight:bold;">Quit playing with the html of this file</p>
